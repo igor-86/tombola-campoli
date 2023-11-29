598c93e56fdc84641a27ffe834213ec8406688da
igor-86 <i.campoli86@gmail.com>
2023-11-30 00:02:42 +0100

    Primo commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="menu">
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">Tombola</router-link>
+    </nav>
+  </div>
+  <router-view />
+  <footer>
+    <h3>il footer</h3>
+  </footer>
 </template>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -15,16 +26,38 @@
   color: #2c3e50;
 }
 
+.menu {
+  width: 100%;
+  padding: 50px 0;
+  background-color: #A63F03;
+}
+
 nav {
-  padding: 30px;
+  width: 50%;
+  display: flex;
+  justify-content: space-around;
+  margin: 0 auto;
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: white;
+  text-decoration: none;
+
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: rgb(184, 184, 57);
+}
+
+footer {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+}
+
+footer h3 {
+  text-align: center;
 }
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,55 @@
+<!-- TabelloneTombola.vue -->
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>Tabellone Tombola</h1>
+    <div class="row" v-for="(row, rowIndex) in tabellone" :key="rowIndex">
+      <div class="numero" v-for="(numero, columnIndex) in row" :key="columnIndex">
+        {{ numero }}
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      tabellone: this.generaTabellone()
+    };
+  },
+  methods: {
+    generaTabellone() {
+      const numeri = Array.from({ length: 90 }, (_, index) => index + 1);
+      const tabellone = [];
+
+      for (let i = 0; i < 9; i++) {
+        const row = numeri.slice(i * 10, (i + 1) * 10);
+        tabellone.push(row);
+      }
+
+      return tabellone;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.row {
+  display: flex;
+  width: 60%;
+  height: 10%;
+  margin: 0 auto;
+}
+
+.numero {
+  width: calc(100% / 10);
+  height: calc(100% / 9);
+  border: 1px solid #A63F03;
+  padding: 10px;
+  text-align: center;
+  color: #A63F03;
+  margin-right: .7rem;
+  margin-bottom: .7rem;
+}
+</style>
+
